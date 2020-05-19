c36aaf0ffca180611273553b2cc217814cce85f6
Joseledgy <jrl22ledger@gmail.com>
2020-05-19 23:31:28 +0300

    added wrapper for invoice/create

--- pages/invoices/create.vue
+++ pages/invoices/create.vue
@@ -1,50 +1,52 @@
 <template>
-    <div>
-        <v-toolbar dense>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-layout>
+        <v-flex class="text-center">
+            <v-toolbar dense>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-            <v-toolbar-title>Create An Invoice</v-toolbar-title>
+                <v-toolbar-title>Create An Invoice</v-toolbar-title>
 
-            <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-            <v-btn icon>
-                <v-icon>mdi-magnify</v-icon>
-            </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-magnify</v-icon>
+                </v-btn>
 
-            <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-            </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                </v-btn>
 
-            <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-        </v-toolbar>
+                <v-btn icon>
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+            </v-toolbar>
 
-        <v-row>
-            <draggable-nested v-model="elements" />
-        </v-row>
-    
-        <v-bottom-navigation
-            v-model="bottomNav"
-            absolute
-            height="40"
-        >
-            <v-btn value="recent">
-                <span>Recent</span>
-                <v-icon>mdi-history</v-icon>
-            </v-btn>
+            <v-row>
+                <draggable-nested v-model="elements" />
+            </v-row>
+        
+            <v-bottom-navigation
+                v-model="bottomNav"
+                absolute
+                height="40"
+            >
+                <v-btn value="recent">
+                    <span>Recent</span>
+                    <v-icon>mdi-history</v-icon>
+                </v-btn>
 
-            <v-btn value="favorites">
-                <span>Favorites</span>
-                <v-icon>mdi-heart</v-icon>
-            </v-btn>
+                <v-btn value="favorites">
+                    <span>Favorites</span>
+                    <v-icon>mdi-heart</v-icon>
+                </v-btn>
 
-            <v-btn value="nearby">
-                <span>Nearby</span>
-                <v-icon>mdi-map-marker</v-icon>
-            </v-btn>
-        </v-bottom-navigation>
-    </div>
+                <v-btn value="nearby">
+                    <span>Nearby</span>
+                    <v-icon>mdi-map-marker</v-icon>
+                </v-btn>
+            </v-bottom-navigation>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
