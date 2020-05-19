895daac988d8445ad8ac0f6f977ead5733251549
Joseledgy <jrl22ledger@gmail.com>
2020-05-19 14:34:01 +0300

    Added Invoice Pages; Remove some preloaded codes; lay out the design of general pages; edit website description; change the theme from dark

---
+++ pages/invoices/create.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <v-toolbar dense>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Create An Invoice</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-toolbar>
+    
+        <v-bottom-navigation
+            v-model="bottomNav"
+            absolute
+            height="40"
+        >
+            <v-btn value="recent">
+                <span>Recent</span>
+                <v-icon>mdi-history</v-icon>
+            </v-btn>
+
+            <v-btn value="favorites">
+                <span>Favorites</span>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn value="nearby">
+                <span>Nearby</span>
+                <v-icon>mdi-map-marker</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            bottomNav: 'recent',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
