a0343f412f1f4b44839168755deb05fbddaf1630
Joseledgy <jrl22ledger@gmail.com>
2020-06-03 18:51:47 +0300

    can now extract and display child elements, then adding identation to identify as child elements, made a recursive function for extracting content for the body; create a function for formatting PDF for view

--- pages/invoices/_id/index.vue
+++ pages/invoices/_id/index.vue
@@ -1,13 +1,18 @@
 <template>
     <v-row no-gutters="">
         <v-col>
-            <v-card>
+            <v-card
+                dark>
 
-                <div v-show="pageCount > 1">
-                    <v-btn x-small class="mx-1 move-prev" fab fixed right bottom @click="movePage('prev')" color="primary">
+                <v-btn x-small class="mx-1 print-form" fab fixed right bottom @click="$refs.pdfComponent.print()" color="primary">
+                    <v-icon dark>mdi-printer</v-icon>
+                </v-btn>
+                
+                <div class="move-page" v-show="pageCount > 1">
+                    <v-btn x-small class="mx-1 move-prev" fab @click="movePage('prev')" color="primary">
                         <v-icon dark>mdi-chevron-left</v-icon>
                     </v-btn>
-                    <v-btn x-small class="mx-1" fab fixed right bottom @click="movePage('next')" color="primary">
+                    <v-btn x-small class="mx-1" fab @click="movePage('next')" color="primary">
                         <v-icon dark>mdi-chevron-right</v-icon>
                     </v-btn>
                 </div>
@@ -19,6 +24,7 @@
                 <PDFRender
                     :src="generatePDF"
                     :page="currentPage"
+                    ref="pdfComponent"
                     @num-pages="pageCount = $event"
                     @page-loaded="currentPage = $event"
                 ></PDFRender>
@@ -50,13 +56,25 @@ export default {
     },
     methods: {
         fillBody() {
-            let _temp_records = [];
-            this.invoice.layout.forEach(elem => {
+            this.body = this.extractLayout(this.invoice.layout);
+        },
+        fillFooter() {
+            let _footer = [];
+            _footer[0] = [
+                { content: `${this.toWords.convert(this.total)} Qatar Riyals Only`, colSpan: 4, styles: { halign: 'left' } },
+                { content: this.numberWithCommas(this.total.toFixed(2)), styles: { halign: 'right' }}
+            ];
+            this.footer = _footer;
+        },
+        extractLayout(layout, len = 0) {
+            let _records = [];
+
+            layout.forEach(elem => {
                 let _temp_object = {};
                 _temp_object.key = elem.key; //key
 
                 if (elem.source.hasOwnProperty('content'))
-                    _temp_object.description = this.viewFormat(elem.source.content);
+                    _temp_object.description = this.viewFormat(`${elem.source.content}`, { newline: true, tab: len });
                 else if (elem.source.hasOwnProperty('origin')) {
                     let _item = this.getSourceData(this.invoice, elem.source.origin.split('/'));
 
@@ -68,17 +86,13 @@ export default {
                 }
 
                 //console.log(_temp_object)
-                _temp_records.push(_temp_object);
+                _records.push(_temp_object);
+                
+                if (elem.children.length > 0)
+                    _records = _records.concat(this.extractLayout(elem.children, len+=1));
             });
-            this.body = _temp_records;
-        },
-        fillFooter() {
-            let _footer = [];
-            _footer[0] = [
-                { content: `${this.toWords.convert(this.total)} Qatar Riyals Only`, colSpan: 4, styles: { halign: 'left' } },
-                { content: this.numberWithCommas(this.total.toFixed(2)), styles: { halign: 'right' }}
-            ];
-            this.footer = _footer;
+
+            return _records;
         },
         tempBodyData() {
             let _data = [
@@ -133,9 +147,22 @@ export default {
                     this.currentPage++
             }
         },
-        viewFormat(txt) {
+        viewFormat(txt, opt = null) {
             let formattedText = txt;
-            formattedText = formattedText.replace(/\\n/g, '\n');
+
+            if (opt === null) return txt;
+            
+            if (opt.hasOwnProperty('newline') && opt.newline)
+                formattedText = formattedText.replace(/\\n/g, '\n');
+            
+            if (opt.hasOwnProperty('tab') && opt.tab > 0) { //indent "2" spaces
+                let _tab = '';
+                for (let i = 0; i < opt.tab; i++) {
+                    _tab += '  ';
+                }
+                formattedText = _tab + formattedText;
+            }
+                
             return formattedText;
         }
     },
@@ -144,6 +171,7 @@ export default {
     },
     created: function() {
         this.fillBody(); //set body
+        //this.tempBodyData();
         this.fillFooter();
     },
     computed: {
@@ -153,17 +181,8 @@ export default {
         generatePDF() {
             let doc = new jsPDF(); //default => unit: 'mm', format: 'a4',
 
-            // doc.setFontSize(18);
-            // doc.text(`Goodwill Electrical & Mechanical Services`, 50, 22);
-
-            // doc.setFontSize(13);
-            // doc.text(`Doha, Qatar`, 90, 28);
-
-            // doc.setFontSize(15);
-            // doc.text(`BANK PAYMENT VOUCHER`, 65, 34);
-
-            doc.setFontSize(7);
-            doc.text(`(Edge)`, 1, 1);
+            // doc.setFontSize(7);
+            // doc.text(`(Edge)`, 1, 1);
 
             doc.setFontSize(11);
             doc.setFont('Helvetica', 'Bold');
@@ -213,13 +232,9 @@ export default {
             doc.line(40, doc.lastAutoTable.finalY + 20, 74, doc.lastAutoTable.finalY + 20);
             doc.text(`Authorized Signature`, 160, doc.lastAutoTable.finalY + 20);
             
-            doc.rect(14, 14, 182, doc.lastAutoTable.finalY - 14); //border
-            console.log(doc.lastAutoTable)
-            console.log(doc.lastAutoTable.finalY - doc.lastAutoTable.pageStartY)
-            console.log(doc.lastAutoTable.height)
-            console.log(doc.lastAutoTable.height - (doc.lastAutoTable.headHeight + doc.lastAutoTable.footHeight))
+            //doc.rect(14, 14, 182, doc.lastAutoTable.finalY - 14); //border
+            //console.log(doc.lastAutoTable); //info about the last drawn table
             
-            console.log(doc.getFontList());
             doc.autoPrint({ variant: 'non-conform' });
             let _pdf = doc.output('datauristring');
             return _pdf;
@@ -232,7 +247,14 @@ export default {
 </script>
 
 <style scoped>
+    .move-page {
+        position:               fixed;
+        right:                  64px;
+        bottom:                 16px;
+        z-index:                4;
+    }
     .move-prev {
-        right:                  60px;
+    }
+    .print-form {
     }
 </style>
