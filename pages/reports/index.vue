ce3ed7d61914ccf5dab4ec8179fba10347d142eb
Joseledgy <jrl22ledger@gmail.com>
2021-05-26 08:46:55 +0300

    In reports, created the structure for the page, how the contents of the page is laid out and how the experience is going to be. Each category has its own section, containing the title and below the reports pertaining to users needs.

--- pages/reports/index.vue
+++ pages/reports/index.vue
@@ -2,14 +2,55 @@
     <v-container>
         <v-row no-gutters>
             <v-col>
-                <v-card>
+
+				<div class="text-h4 my-5" v-text="'Reports'"></div>
+
+                <v-card class="mb-9 mx-md-5">
                     <v-card-title>Company Assets, Profits</v-card-title>
                     <v-list>
                         <v-list-item-group>
                             <v-list-item>
                                 <v-list-item-content>
                                     <v-list-item-title v-text="'Balance Sheet'"></v-list-item-title>
+                                    <v-list-item-subtitle v-text="'To calculate the profit made by the company, a summary of revenue and expenditures must be provided.'"></v-list-item-subtitle>
                                 </v-list-item-content>
+                                <v-list-item-icon>
+                                    <v-icon v-text="'mdi-chevron-right'"></v-icon>
+                                </v-list-item-icon>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-card>
+
+                <v-card class="mb-9 mx-md-5">
+                    <v-card-title>Customers' Accounts</v-card-title>
+                    <v-list>
+                        <v-list-item-group>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="'Income by Customer'"></v-list-item-title>
+                                    <v-list-item-subtitle v-text="'Customer breakdown of paid and unpaid wages.'"></v-list-item-subtitle>
+                                </v-list-item-content>
+                                <v-list-item-icon>
+                                    <v-icon v-text="'mdi-chevron-right'"></v-icon>
+                                </v-list-item-icon>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-card>
+
+                <v-card class="mb-9 mx-md-5">
+                    <v-card-title>Bookkeeping</v-card-title>
+                    <v-list>
+                        <v-list-item-group>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="'Account Transactions (General Ledger)'"></v-list-item-title>
+                                    <v-list-item-subtitle v-text="'All transactions are listed in detail, along with their totals, by account - Every transactions in the Chart of Account.'"></v-list-item-subtitle>
+                                </v-list-item-content>
+                                <v-list-item-icon>
+                                    <v-icon v-text="'mdi-chevron-right'"></v-icon>
+                                </v-list-item-icon>
                             </v-list-item>
                         </v-list-item-group>
                     </v-list>
